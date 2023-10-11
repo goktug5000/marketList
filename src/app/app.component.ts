e1a35f5987c68c5b9c42bad88c416a05fe80aac3
goktug5000 <goktug5000@gmail.com>
2023-10-11 17:43:05 +0300

    holder ekledim

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ListTemp,ListTempHolder,ListItem } from './listTemp';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+ 
   title = 'marketList';
+  listTemp = new ListTemp("goktug");
+  listTempHolder=new ListTempHolder();
+
+  ngOnInit(): void {
+    console.log(this.listTempHolder.listTemps[0]);
+  }
+  getHolder(){
+    return this.listTempHolder.listTemps;
+  }
+  getOwner(){
+    return this.listTemp.listOwner;
+  }
+  getItems(){
+    return this.listTemp.itemss;
+  }
 }
---
+++ src/app/listTemp.ts
@@ -0,0 +1,31 @@
+export class ListTemp{
+    listOwner:any;
+    itemss:any;
+    constructor(listOwnerr:any){
+        this.listOwner=listOwnerr;
+        this.itemss=[
+            new ListItem("su",2),
+            new ListItem("ekmek",4),
+            new ListItem("elma",3),
+        ]
+    }
+}
+export class ListItem{
+    name;
+    amount;
+    constructor(namee:any,amountt:any){
+        
+        this.name=namee;
+        this.amount=amountt;
+    }
+}
+export class ListTempHolder{
+
+    listTemps:any;
+    constructor(){
+        this.listTemps=[
+            new ListTemp("goktug"),
+            new ListTemp("ahmet")
+        ]
+    }
+}
